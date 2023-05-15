a8c5cb5b7e3de4c86a57f387d8bbf4302476b481
juanmara9305 <jumara_93@hotmail.com>
2023-05-15 13:57:05 -0500

    Clase 4 - Rutas

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,3 +1,4 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import './App.css'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
@@ -5,18 +6,29 @@ import MyOrders from '../MyOrders'
 import MyOrder from '../MyOrder'
 import NotFound from '../NotFound'
 import Signin from '../Signin'
+import '../App/App.css'
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    {path: '/', element: <Home />},
+    {path: '/my-account',element: <MyAccount />},
+    {path: '/my-orders',element: <MyOrders />},
+    {path: '/my-order',element: <MyOrder />},
+    {path: '/*',element: <NotFound />},
+    {path: '/sign-in',element: <Signin />}
+  ]);
+
+  return routes;
+};
 
 function App() {
+  
 
   return (
-    <div className='bg-red-100'>
-      <Home/>
-      <MyAccount/>
-      <MyOrders/>
-      <MyOrder/>
-      <Signin/>
-      <NotFound/>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+      
+    </BrowserRouter>
   )
 }
 
