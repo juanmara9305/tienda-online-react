2e84f80c57152479b6d659f478c192229d5241e2
juanmara9305 <jumara_93@hotmail.com>
2023-05-17 11:18:31 -0500

    Clase 12 - Add heroicons

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -20,7 +20,9 @@ const  Card = (data) => {
                 className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
                 onClick={() => context.setCount(context.count+1)}
             >
-                +
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-blue-600">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
             </div>
         </figure>
         <p className="flex justify-between">
--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -4,7 +4,9 @@ const ProductDetail = () => {
         <aside className="product-detail flex flex-col fixed right-0 border-black rounded-lg bg-white">
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>Detail</h2>
-                <div>x</div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
             </div>                            
         </aside>
     )
